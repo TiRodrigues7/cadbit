0cf6581bbe398ca034ea70e5c08cc441a9c13543
TiRodrigues7 <130255170+TiRodrigues7@users.noreply.github.com>
2025-08-02 22:11:46 -0300

    Create Site Cadbit

---
+++ index.php
@@ -0,0 +1,126 @@
+<!DOCTYPE HTML>
+<!--
+	Lens by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html lang="pt-br">
+<html>
+	<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta http-equiv="viewport" content="width=device-width, initial-scale=1.0">
+		<title>CadBit</title>
+		<link rel="shortcut icon" href="images/icone-logo.png" type="image/x-icon">
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<link rel="stylesheet" href="assets/css/style.css" />
+		<link rel="stylesheet" href="assets/css/fontawesome.min.css" />
+		<link rel="stylesheet" href="assets/css/all.min.css" />
+	</head>
+	<body class="is-preload-0 is-preload-1 is-preload-2">
+
+		<!-- Main -->
+			<div id="main" > <!-- class="back-efeito"> -->
+				<img src="images/thumbs/logo.png" class="img-logo" />
+				<!-- Header -->
+					<header id="header" >
+						<div class="gradient-efeito margin-left-cabe ">
+							CadBit
+						</div>
+						<p class="margin-left-cabe1">Solução em Mídias Sociais & TI <br><a class="gradient-efeito-01 " href="https://cadbit.com.br">01|01</a></p>
+						<ul class="icons">
+							<li><a href="https://www.facebook.com/cadbit01" class="icon brands fa-facebook"><span class="label">Twitter</span></a></li>
+							<li><a href="https://www.instagram.com/cadbit01/" class="icon brands fa-instagram"><span class="label">Instagram</span></a></li>
+							<li><a href="https://wa.me/+5514996758455?text=Olá%20tudo%20bem?%20Gostaria%20de%20mais%20informações%20sobre%20" class="icon brands fa-brands fa-square-whatsapp"><span class="label">Wahts</span></a></li>
+							<!-- <li><a href="#" class="icon brands fa-github"><span class="label">Github</span></a></li> -->
+							<!-- <li><li><a href="https://www.facebook.com/cadbit01" class="icon brands fa-brands fa-square-facebook"><span class="label">Facebook</span></a></li> -->
+							<li><a href="mailto: cadbit@cadbit.com.br" class="icon fa-envelope"><span class="label">Email</span></a></li>
+						</ul>
+					</header>
+
+				<!-- Thumbnail -->
+					<section id="thumbnails">
+						<article>
+							<a class="thumbnail" href="images/fulls/07.png" data-position="left center"><img src="images/thumbs/07.png" alt="" /></a>
+							<h2><a href="https://wa.me/+5514996841651?text=Ol%C3%A1%20tudo%20bem?%20Gostaria%20de%20mais%20informa%C3%A7%C3%B5es%20sobre%20" class="botao-cad" >SAIBA MAIS</a></h2>
+						</article>
+						<article>
+							<a class="thumbnail" href="images/fulls/08.png" data-position="left center"><img src="images/thumbs/08.png" alt="" /></a>
+							<h2><a href="https://wa.me/+5514997334242?text=Ol%C3%A1%20tudo%20bem?%20Gostaria%20de%20mais%20informa%C3%A7%C3%B5es%20sobre%20" class="botao-cad" >SAIBA MAIS</a></h2>
+						</article>
+						<article>
+							<a class="thumbnail" href="images/fulls/09.png" data-position="left center"><img src="images/thumbs/09.png" alt="" /></a>
+							<h2><a href="https://wa.me/+5514997158889?text=Ol%C3%A1%20tudo%20bem?%20Gostaria%20de%20mais%20informa%C3%A7%C3%B5es%20sobre%20" class="botao-cad" >SAIBA MAIS</a></h2>
+						</article>
+						<article>
+							<a class="thumbnail" href="images/fulls/01.png" data-position="left center"><img src="images/thumbs/01.png" alt="" /></a>
+							<h2><a href="https://gostosurasdoti.cadbit.com.br" class="botao-cad" >SAIBA MAIS</a></h2>
+						</article>
+						<article>
+							<a class="thumbnail" href="images/fulls/02.png" data-position="left center"><img src="images/thumbs/02.png" alt="" /></a>
+							<h2><a href="https://barberrodrigues.cadbit.com.br" class="botao-cad" >SAIBA MAIS</a></h2>
+						</article>
+						<article>
+							<a class="thumbnail" href="images/fulls/03.png" data-position="left center"><img src="images/thumbs/03.png" alt="" /></a>
+							<h2><a href="https://www.sedestaque.com/" class="botao-cad" >SAIBA MAIS</a></h2>
+						</article>
+						<article>
+							<a class="thumbnail" href="images/fulls/05.png" data-position="left center"><img src="images/thumbs/05.png" alt="" /></a>
+							<h2><a href="https://qualiservourinhos.com.br/" class="botao-cad" >SAIBA MAIS</a></h2>
+						</article>
+						<article>
+							<a class="thumbnail" href="images/fulls/06.png" data-position="left center"><img src="images/thumbs/06.png" alt="" /></a>
+							<h2><a href="https://hidraulicama.com.br/" class="botao-cad" >SAIBA MAIS</a></h2>
+						</article>
+						<article>
+							<a class="thumbnail" href="images/fulls/04.png" data-position="left center"><img src="images/thumbs/04.png" alt="" /></a>
+							<h2><a href="https://cadbit.com.br/" class="botao-cad" >CADBIT 01</a></h2>
+						</article>
+						<!-- <article>
+							<a class="thumbnail" href="images/fulls/06.png" data-position="left center"><img src="images/thumbs/06.png" alt="" /></a>
+							<h2><a href="https://cadbit.com.br" class="botao-cad" >CADBIT 01</a></h2>
+						</article>
+						<article>
+							<a class="thumbnail" href="images/fulls/07.png" data-position="left center"><img src="images/thumbs/07.png" alt="" /></a>
+							<h2><a href="http://www.compreoualuguefacil.com.br" class="botao-cad" >SAIBA MAIS</a></h2>
+						</article>
+						<article>
+							<a class="thumbnail" href="images/fulls/08.png" data-position="left center"><img src="images/thumbs/08.png" alt="" /></a>
+							<h2><a href="https://barberrodrigues.cadbit.com.br" class="botao-cad" >SAIBA MAIS</a></h2>
+						</article>
+						<article>
+							<a class="thumbnail" href="images/fulls/09.png" data-position="left center"><img src="images/thumbs/09.png" alt="" /></a>
+							<h2><a href="https://gostosurasdoti.cadbit.com.br" class="botao-cad" >SAIBA MAIS</a></h2>
+						</article>
+						<article>
+							<a class="thumbnail" href="images/fulls/10.png" data-position="left center"><img src="images/thumbs/10.png" alt="" /></a>
+							<h2><a href="http://www.compreoualuguefacil.com.br" class="botao-cad" >SAIBA MAIS</a></h2>
+						</article>
+						<article>
+							<a class="thumbnail" href="images/fulls/11.png" data-position="left center"><img src="images/thumbs/11.png" alt="" /></a>
+							<h2><a href="https://barberrodrigues.cadbit.com.br" class="botao-cad" >SAIBA MAIS</a></h2>
+						</article>
+						<article>
+							<a class="thumbnail" href="images/fulls/12.png" data-position="left center"><img src="images/thumbs/12.png" alt="" /></a>
+							<h2><a href="https://cadbit.com.br" class="botao-cad" >CADBIT 01</a></h2>
+						</article> -->
+					</section>
+
+				<!-- Footer -->
+					<footer id="footer">
+						<ul class="copyright">
+							<li>&copy; CadBit.</li><li>Design: <a href="https://www.linkedin.com/in/tiago-ar86/">Tiago Rodrigues</a>.</li>
+						</ul>
+					</footer>
+
+			</div>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/browser.min.js"></script>
+			<script src="assets/js/breakpoints.min.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
