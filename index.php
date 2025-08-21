40ea088b9d11db870c7a0b17939478039d2d92fc
TiRodrigues7 <130255170+TiRodrigues7@users.noreply.github.com>
2025-08-21 19:58:12 -0300

    Update index.php
    :wheelchair: CadBit

--- index.php
+++ index.php
@@ -53,6 +53,7 @@
 							<a class="thumbnail" href="images/fulls/09.png" data-position="left center"><img src="images/thumbs/09.png" alt="" /></a>
 							<h2><a href="https://wa.me/+5514997158889?text=Ol%C3%A1%20tudo%20bem?%20Gostaria%20de%20mais%20informa%C3%A7%C3%B5es%20sobre%20" class="botao-cad" >SAIBA MAIS</a></h2>
 						</article>
+						<!--
 						<article>
 							<a class="thumbnail" href="images/fulls/01.png" data-position="left center"><img src="images/thumbs/01.png" alt="" /></a>
 							<h2><a href="https://gostosurasdoti.cadbit.com.br" class="botao-cad" >SAIBA MAIS</a></h2>
@@ -61,23 +62,25 @@
 							<a class="thumbnail" href="images/fulls/02.png" data-position="left center"><img src="images/thumbs/02.png" alt="" /></a>
 							<h2><a href="https://barberrodrigues.cadbit.com.br" class="botao-cad" >SAIBA MAIS</a></h2>
 						</article>
-						<article>
-							<a class="thumbnail" href="images/fulls/03.png" data-position="left center"><img src="images/thumbs/03.png" alt="" /></a>
-							<h2><a href="https://www.sedestaque.com/" class="botao-cad" >SAIBA MAIS</a></h2>
-						</article>
+						-->
 						<article>
 							<a class="thumbnail" href="images/fulls/05.png" data-position="left center"><img src="images/thumbs/05.png" alt="" /></a>
-							<h2><a href="https://qualiservourinhos.com.br/" class="botao-cad" >SAIBA MAIS</a></h2>
+							<h2><a href="https://qualiserv.cadbit.com.br/" class="botao-cad" >SAIBA MAIS</a></h2>
 						</article>
 						<article>
 							<a class="thumbnail" href="images/fulls/06.png" data-position="left center"><img src="images/thumbs/06.png" alt="" /></a>
-							<h2><a href="https://hidraulicama.cadbit.com.br/" class="botao-cad" >SAIBA MAIS</a></h2>
+							<h2><a href="https://www.hidraulicama.cadbit.com.br/" class="botao-cad" >SAIBA MAIS</a></h2>
 						</article>
 						<article>
 							<a class="thumbnail" href="images/fulls/04.png" data-position="left center"><img src="images/thumbs/04.png" alt="" /></a>
 							<h2><a href="https://cadbit.com.br/" class="botao-cad" >CADBIT 01</a></h2>
 						</article>
-						<!-- <article>
+						<!-- 
+						<article>
+							<a class="thumbnail" href="images/fulls/03.png" data-position="left center"><img src="images/thumbs/03.png" alt="" /></a>
+							<h2><a href="https://www.sedestaque.com/" class="botao-cad" >SAIBA MAIS</a></h2>
+						</article>
+						<article>
 							<a class="thumbnail" href="images/fulls/06.png" data-position="left center"><img src="images/thumbs/06.png" alt="" /></a>
 							<h2><a href="https://cadbit.com.br" class="botao-cad" >CADBIT 01</a></h2>
 						</article>
@@ -125,3 +128,4 @@
 	</body>
 
 </html>
+
